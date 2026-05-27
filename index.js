329e82b772d6a7f65667db410ed069edc0e34ce8
Baptiste <baptiste.buquet@gmail.com>
2026-05-27 11:46:24 +0200

    landing page + app séparée

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+app.get('/app', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/app.html'));
+});
+
 const path = require('path');
 app.get('/hljs/highlight.min.js', (req, res) => {
   res.sendFile(path.join(__dirname, 'node_modules/highlight.js/lib/core.js'));
